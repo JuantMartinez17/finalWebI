699174f50c9c65c716c1acc822cd00e317012760
JuantMartinez17 <JuantMartinez17>
2024-07-10 18:13:44 -0300

    login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit(): void{
+    this.authService.login(this.email, this.password).subscribe(response => {
+      if(response.success){
+        (<any>this.router).navigate(['/admin']);
+      }else{
+        alert('login failed');
+      }
+    });
+  }
 
 }
